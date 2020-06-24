131ad53501aa7d22aa64efa4d0eb5e1a9c7d7d75
Masinhyeong <msh1229@kunsan.ac.kr>
2020-06-25 02:23:21 +0900

    marker

--- main.js
+++ main.js
@@ -10,17 +10,11 @@ var db = require('./lib/db');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
-app.use(express.static('public'));
-app.get('*', function(request, response, next){
-  fs.readdir('./data', function(error, filelist){
-    request.list = filelist;
-    next();
-  });
-});
+
 
 app.get('/', function(request, response) {
   var title = 'fishing';
-  var list = func.list(request.list);
+  var list = func.list();
   var html = func.MHTML(list);
   response.send(html);
 });
@@ -28,49 +22,15 @@ app.use(express.static('public'));
 app.get('/page/information', function(request, response) {
   var title ='';
   var list ='';
-  db.query('select * from 장소 where 지역 = "모항";'+'select * from 배 where blocate = "모항";'+'select * from 숙박 where mlocation = "모항";'+'select * from 편의시설 where plocation = "모항";', function (error, results, fields) {
-    if (error) {
-        console.log(error);
-    }else{
-      
-      b=results[0][0].x;
-      c=results[0][0].y;
-      
-    var boat = ``;
-    var i = 0;
-    while(i < results[1].length){
-      boat = boat + `<div class = boxer><h2>${results[1][i].bname}<h2></br><h3>${results[1][i].bnumber}</h3></div>`;
-      i = i + 1;
-    };
-
-    var motel = ``;
-    var i = 0;
-    while(i < results[2].length){
-      
-      motel = motel + `<div class = boxer><h2>${results[2][i].mname}<h2></br><h3>${results[2][i].mnumber}</h3></div>`;
-      i = i + 1;
-    };
-
-    var pss = ``;
-    var i = 0;
-    while(i < results[3].length){
-      pss = pss + `<div class = boxer><h2>${results[3][i].pname}<h2></br><h3>${results[3][i].pnumber}</h3></div>`;
-      i = i + 1;
-    }
-  
-    
-    
-      title= 'fishing';
-      list = func.list(request.list);
-      var html = func.HTML2(list,b,c,boat, motel,pss);
+  title= 'fishing';
+      var list = func.list();
+      var html = func.HTML3(list);
       response.send(html); 
-    }
- });
 
 });
 
 app.post('/page/information',function(req,res){
- var a = req.body.locate;
+ var a = req.body.selThree;
 
  var title ='';
  var list ='';
@@ -107,7 +67,7 @@ app.post('/page/information',function(req,res){
        mk = mk+`var marker = new google.maps.Marker({
         position: new google.maps.LatLng(${results[3][i].x},${results[3][i].y}),
         map: map,
-        title: 'Seoul Station'
+        title: '${results[3][i].pname}'
       });`
        
        i = i + 1;
@@ -115,24 +75,18 @@ app.post('/page/information',function(req,res){
    
    
    
-     title= 'fishing';
-     list = '';
-     var html = func.HTML2(list,b,c,boat, motel,pss,mk);
+     var title= 'fishing';
+     var list = func.list();
+     var html = func.HTML2(list,a,b,c,boat, motel,pss,mk);
      res.send(html); 
    }
 });
 });
 
 app.get('/page/Q&A', function(request, response) {
-    
-  // var filteredId = path.parse(request.params.call).base;
-  // fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-
-    fs.readFile(`data/Q&A`, 'utf8', function(err, description){
-    var title = 'fishing';
-    var list = func.list(request.list);
-    var html = func.HTML(list, description);
-    response.send(html);
-    });
+  var title = 'fishing';
+  var list = func.list();
+  var html = func.QHTML(list);
+  response.send(html);
 });
 app.listen(3000);
